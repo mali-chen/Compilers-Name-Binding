377c65278099d63558575d84d2eef3a6227092a2
chengu26 <chengu26@up.edu>
2026-03-07 21:51:40 -0800

    sem2visitor

--- asst3Starter/asst3Starter/src/visitor/Sem2Visitor.java
+++ asst3Starter/asst3Starter/src/visitor/Sem2Visitor.java
@@ -1,8 +1,8 @@
 package visitor;
 
-import syntaxtree.*;
-import java.util.*;
 import errorMsg.*;
+import java.util.*;
+import syntaxtree.*;
 
 // the purpose of this class is to
 // - link each ClassDecl to the ClassDecl for its superclass 
@@ -21,6 +21,62 @@ public class Sem2Visitor extends Visitor
     {
         errorMsg = e;
         classEnv = env;
+    }
+
+    @Override
+    public Object visit(Program p){
+        // link every class to its superclass
+        for(ClassDecl n : p.classDecls){
+            visit(n);
+        }
+
+        // cycle deteftion over same list
+        for(ClassDecl n : p.classDecls){
+            detectCycle(n);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Object visit(ClassDecl n){
+        // skip class that have no superclass
+        if(n.superName.equals("")){
+            return null;
+        }
+
+        // check for illegal superclass names
+        if(n.superName.equals("String")|| n.superName.equals("RunMain")){
+            errorMsg.error(n.pos, CompError.IllegalSuperclass(n.superName));
+            return null;
+        }
+
+        //look up superclass by name
+        ClassDecl superClass = classEnv.get(n.superName);
+        if(superClass == null){
+            errorMsg.error(n.pos, CompError.UndefinedSuperclass(n.superName));
+            return null;
+        }
+
+        n.superLink = superClass;
+        superClass.subclasses.add(n);
+
+        return null;
+    }
+
+    private void detectCycle(ClassDecl n){
+        HashSet<ClassDecl> visited = new HashSet<ClassDecl>();
+        ClassDecl curr = n;
+
+        while(curr != null && !curr.superName.equals("")){
+            // if seen this node before, found cycle
+            if(visited.contains(curr)){
+                errorMsg.error(n.pos, CompError.InheritanceCycle(n.name));
+                return;
+            }
+            visited.add(curr);
+            curr = curr.superLink;
+        }
     }
 
 }
